0de314e0054f022e18aab1ce2eccde4e2b2923cd
Guy Sartorelli <guy.sartorelli@gmail.com>
2025-05-26 12:35:31 +1200

    FIX Don't require deprecation for constructor with no params

--- tests/fixture-code/cloned/from/vendor/some-org/module2/src/Something/ClassTwo.php
+++ tests/fixture-code/cloned/from/vendor/some-org/module2/src/Something/ClassTwo.php
@@ -4,6 +4,11 @@ namespace SomeOrg\Module2\Something;
 
 class ClassTwo
 {
+    public function __construct()
+    {
+        // note, no params in this one
+    }
+
     public function someMethod(array $returnMe = []): array
     {
         return $returnMe;
